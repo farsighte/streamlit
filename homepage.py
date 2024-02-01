6057fc05d781fa1e47b11731ee02da55aa9088fe
farsighte <92359132+farsighte@users.noreply.github.com>
2024-02-01 20:28:40 +0900

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -5,7 +5,7 @@ st.title("나의 Streamlit 앱")
 
 # 상단 바에 메뉴를 추가할 수 있습니다.
 menu = ["홈", "페이지1", "페이지2", "페이지3"]
-choice = st.beta_container().radio("메뉴", menu)
+choice = st.sidebar.selectbox("메뉴", menu)
 
 # 선택한 메뉴에 따라 다른 내용을 표시할 수 있습니다.
 if choice == "홈":
@@ -16,3 +16,4 @@ elif choice == "페이지2":
     st.write("이곳은 페이지 2입니다.")
 elif choice == "페이지3":
     st.write("이곳은 페이지 3입니다.")
+
